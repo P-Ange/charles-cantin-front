2e93638fcbc6dc60d80525cccfef017641ff9f3f
P-ange <pascal.cretinlange@gmail.com>
2021-12-28 11:37:36 +0100

    MAJ Files BIS

--- components/header.js
+++ components/header.js
@@ -72,8 +72,8 @@ constructor(props) {
 
                   <a passHref
                     target="_blank"
-                    href={this.state.informations.Instagram}
                     rel="noreferrer"
+                    href={this.state.informations.Instagram}
                     ><svg version="1.0" xmlns="http://www.w3.org/2000/svg"
                   width="16.000000pt" height="16.000000pt" viewBox="0 0 16.000000 16.000000"
                   preserveAspectRatio="xMidYMid meet">
@@ -96,6 +96,7 @@ constructor(props) {
                 
                   <a passHref
                   target="_blank"
+                  rel="noreferrer"
                     href={this.state.informations.Facebook}
                    
                   ><svg version="1.0" xmlns="http://www.w3.org/2000/svg"
